fee0d4adf82ba86459ef4eb4e9b83180763435a2
GoJiMin <gojimin3095@gmail.com>
2024-10-27 23:52:35 +0900

    feat: 도서관 검색 결과 메세지 출력
    - 검색 결과 (resultNum)이 0일 경우, libraryList 상태를 업데이트하지 않고 토스트 메세지를 이용해 안내 메세지 출력.

--- src/features/library/search-library/lib/hooks/useSearchLibrary.ts
+++ src/features/library/search-library/lib/hooks/useSearchLibrary.ts
@@ -8,7 +8,7 @@ export function useSearchLibrary() {
     null
   );
 
-  const { toastError } = useToast();
+  const { toastError, toastInfo } = useToast();
 
   const onSubmit = async (isbn: string, { region, dtlRegion }: RegionState) => {
     if (!region) {
@@ -22,7 +22,15 @@ export function useSearchLibrary() {
       region: region!.value,
       dtl_region: dtlRegion?.value,
     })
-      .then(setLibraryList)
+      .then((res: LibrarySearchResult) => {
+        if (Number(res.resultNum)) {
+          setLibraryList(res);
+          return;
+        }
+
+        toastInfo("해당 지역엔 소장 중인 도서관이 없어요.");
+        return;
+      })
       .catch((error) => toastError(error.message));
   };
 
